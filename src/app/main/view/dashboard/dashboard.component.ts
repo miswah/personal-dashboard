8991f81e1632af8eb88474118a2d6d1ff9e9385a
miswah <miswah.md@gmail.com>
2022-12-18 12:42:33 +0530

    (design): dashboard skeleton

--- src/app/main/view/dashboard/dashboard.component.ts
+++ src/app/main/view/dashboard/dashboard.component.ts
@@ -10,6 +10,11 @@ export class DashboardComponent implements OnInit {
     { id: 1, title: "Android Development", total_task: 35, completed: 13, due_date: "2022-10-31T09:00:00Z", status: "Paused", last_visited: "2022-10-31T09:00:00Z" },
     { id: 2, title: "Web Development", total_task: 15, completed: 20, due_date: "2022-10-31T09:00:00Z", status: "Completed", last_visited: "2022-10-31T09:00:00Z" },
   ];
+
+  public courses = [
+    { id: 1, title: "Android Development", total_task: 35, task_completed: 13, next_task: "Design landing page", due_date: "2022-10-31T09:00:00Z", status: "Paused", last_visited: "2022-10-31T09:00:00Z", last_visited_module: "CSS", project: "BOX PAGE", modules: 20, completed_modules: 10 },
+    { id: 2, title: "Web Development", total_task: 15, task_completed: 20, next_task: "landing page", due_date: "2022-10-31T09:00:00Z", status: "Completed", last_visited: "2022-10-31T09:00:00Z", last_visited_module: "Figma", project: "USER FLOW", modules: 20, completed_modules: 10 },
+  ];
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/main/view/dashboard/dashboard.module.ts
+++ src/app/main/view/dashboard/dashboard.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard.component";
 import { Routes, RouterModule } from "@angular/router";
 import { ProjectModule } from "../project/project.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 //Routing
 const routes: Routes = [{ path: "", component: DashboardComponent }];
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), ProjectModule],
+  imports: [CommonModule, RouterModule.forChild(routes), ProjectModule, NgbModule],
 })
 export class DashboardModule {}
