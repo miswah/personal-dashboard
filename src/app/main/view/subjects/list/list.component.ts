2b0d325cbe339c6b9fc9d5c4c75ad4f2c0f766a3
miswah <miswah.md@gmail.com>
2022-12-20 19:58:13 +0530

    (feat): fetch api for subjects Fetch Api for project list #12

--- src/app/main/view/subjects/list/list.component.ts
+++ src/app/main/view/subjects/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
+import { SubjectsManagementService } from "../subjects-management.service";
 
 @Component({
   selector: "app-list",
@@ -11,9 +12,27 @@ export class ListComponent implements OnInit {
     { id: 1, code: "MCS01", title: "Software Dev", block: 4, total_chapters: 16, completed_chapter: 5, next_chapter: "SDR for center", next_important_date: "2022-10-31T09:00:00Z" },
     { id: 2, code: "MCS01", title: "Software Dev", block: 4, total_chapters: 16, completed_chapter: 5, next_chapter: "SDR for center", next_important_date: "2022-10-31T09:00:00Z" },
   ];
-  constructor(private router: Router) {}
+  constructor(private router: Router, private subjectService: SubjectsManagementService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getAllSubjects();
+  }
+
+  async getAllSubjects(): Promise<void> {
+    //To avoid multiple api calls for same data check if data exists
+    if (this.subjectService.listOfSubjects) {
+      this.subjects = this.subjectService.listOfSubjects;
+      return;
+    }
+
+    let { data: subjects, error } = await this.subjectService.getSubjects();
+    if (error) {
+      console.error("error", error.message);
+    } else {
+      this.subjectService.listOfSubjects = subjects;
+      this.subjects = subjects;
+    }
+  }
 
   redirectToView(id: number) {
     this.router.navigate(["../subject/view", id]);
