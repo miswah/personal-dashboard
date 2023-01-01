11ced01396c23ad2fab87b70bb83196c009e3f13
miswah <miswah.md@gmail.com>
2023-01-01 16:45:37 +0530

    (enh): Change app name to Mission Logs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   hmr: false,
   apiUrl: "http://localhost:4000",
-  title: "test",
+  title: "Mission Logs",
   supabase: {
     url: "https://kpwubhupojzdwzfhjfel.supabase.co",
     key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtwd3ViaHVwb2p6ZHd6ZmhqZmVsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjkyODY0NTgsImV4cCI6MTk4NDg2MjQ1OH0.Jgw1B8BFYfvWzTHzNuecOqEAJeWkXDORbYnEMvWIjFA",
