c82c12d92da526110ba72c45a271db5035049c9f
miswah <miswah.md@gmail.com>
2023-01-19 22:00:23 +0530

    (enh): project interface add

---
+++ src/app/main/view/project/interfaces.ts
@@ -0,0 +1,11 @@
+export interface Project {
+  title: string;
+  priority: string;
+  created_at: string;
+  deadline: string;
+  status: string;
+  last_activity: string;
+  comments: string;
+  description: string;
+  type: string;
+}
