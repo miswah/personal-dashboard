deb2314e6916831ea700204ecd158a858106d079
miswah <miswah.md@gmail.com>
2023-01-19 21:28:41 +0530

    (routing): default page for project is now dashboard

--- src/app/main/view/project/project.module.ts
+++ src/app/main/view/project/project.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from "@angular/common";
 import { ProjectComponent } from "./project.component";
 import { RouterModule, Routes } from "@angular/router";
 import { ListComponent } from "./list/list.component";
-import { CardComponent } from "./card/card.component";
 import { ViewComponent } from "./view/view.component";
 import { environment } from "environments/environment";
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
@@ -11,17 +10,17 @@ import { TimelineComponent } from "./timeline/timeline.component";
 import { jqxKanbanModule } from "jqwidgets-ng/jqxkanban";
 import { ProjectCardComponent } from "./project-card/project-card.component";
 import { SwiperModule } from "ngx-swiper-wrapper";
-import { ProjectDashboardComponent } from './project-dashboard/project-dashboard.component';
+import { ProjectDashboardComponent } from "./project-dashboard/project-dashboard.component";
 
 // Routes
 const routes: Routes = [
-  { path: "list", component: ListComponent, title: "Project - " + environment.title, data: { animation: "project" } },
+  { path: "list", component: ProjectDashboardComponent, title: "Project - " + environment.title, data: { animation: "project" } },
   { path: "view/:id", component: ViewComponent, data: { animation: "project" } },
 ];
 
 @NgModule({
-  declarations: [ProjectComponent, ListComponent, CardComponent, ViewComponent, TimelineComponent, ProjectCardComponent, ProjectDashboardComponent],
+  declarations: [ProjectComponent, ListComponent, ViewComponent, TimelineComponent, ProjectCardComponent, ProjectDashboardComponent],
   imports: [CommonModule, RouterModule.forChild(routes), NgbModule, jqxKanbanModule, SwiperModule],
-  exports: [CardComponent],
+  exports: [ProjectCardComponent],
 })
 export class ProjectModule {}
